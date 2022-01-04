50a59e2de1600d7e6558033b22516cdb32ae4f76
ChildOfJustice <izzvms@gmail.com>
2022-01-04 16:51:00 +0300

    [Major] Fixed the deployment script.

--- utils/Deployment/fullDeploy.sh
+++ utils/Deployment/fullDeploy.sh
@@ -14,22 +14,27 @@ sh packageTemplate.sh $S3BucketName &&
 echo "STEP 2 #### Deploying CloudFormation stack..." &&
 cd ../../Deployment &&
 sh deploy-CF-template.sh $StackName $AwsRegion &&
+aws cloudformation describe-stacks --stack-name $StackName > "stack_description.json" &&
 
 
 echo "STEP 3 #### Generating config file..." &&
-aws cloudformation describe-stacks --stack-name $StackName > "stack_description.json" &&
-
-rm "../../src/config.ts" ||
+rm -f "../../src/config.ts" &&
 python3 ./generate_config_script.py $AwsRegion $S3StorageBucketName &&
 
-rm "amplifyInit.sh" ||
+
+echo "STEP 4 #### Generating amplify initialization file..." &&
+rm -f "amplifyInit.sh" &&
 python3 ./generate_amplify_deployment_script.py $ProjectName &&
 
-echo "STEP 4 #### Initializing amplify app..." &&
+
+echo "STEP 5 #### Initializing amplify app..." &&
+rm -rf "../../amplify" &&
 sh amplifyInit.sh &&
 
-echo "STEP 5 #### Adding hosting to amplify app..." &&
+
+echo "STEP 6 #### Adding hosting to amplify app..." &&
 ./amplifyAddHostingHeadless.sh &&
 
-echo "STEP 6 #### Publishing amplify app..." &&
+
+echo "STEP 7 #### Publishing amplify app..." &&
 sh amplifyPublishHeadless.sh
